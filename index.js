6539c41f5180a3bb54c12a269ae01005cb860994
denny-svg <pknowledge@gmail.com>
2023-12-11 16:49:21 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting =`Welcome, ${currentUser[0]}!`;
+
+'Sarah Wanjiku'[0];
+'Sarah Wanjiku'.chartArt(0);
+
+'Sarah Wanjiku'.length
+'Sarah Wanjiku'[6]
+'Sarah Wanjiku'[13]
+
+
+typeof "const currentUser = 'Grace Hopper';"
+"const currentUser = 'Grace Hopper';".length;
+currentUser;
